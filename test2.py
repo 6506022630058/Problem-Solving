4ca5516ea6bd9a129cb61ecb1a91422dc506461e
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-03-03 12:09:36 +0700

    u

--- test2.py
+++ test2.py
@@ -2,36 +2,32 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-def findpath():
-    for path in nx.all_simple_paths(network, source='a', target='c'):
-       print(path)
+def findpath(src,des):return [path for path in nx.all_simple_paths(network, source=src, target=des)]
 
-def inputlivingthings():
-    name = input('Name: ')
-    energy = input('Energy: ')
-    if name != 'q':
-        inputlivingthings()
-    return name
-
-colist = ["gold","red","violet","pink","green","violet","orange","grey","blue","yellow","cyan"]
-
-network = nx.DiGraph()
-network.add_nodes_from(inputlivingthings())
-print(f"This area has {network.number_of_nodes()} living things.")
+def inputlivingthings(num):return [input(f'Name of living things {i+1}: ').lower() for i in range(num)]
 
 def inputnetwork():
+    print("\n----- Who Eat Who, Ex. --> Tiger Deer\nTo quit --> (Input: q)")
     lis = []
     ans = ''
     while ans != 'q':
-        ans = input("----- Who Eat Who, Ex. --> Tiger Deer\nInput(q to quit): ").lower()
+        ans = input("Input: ").lower()
         if ans != 'q':
             lis.append(tuple(ans.split()))
     return lis
 
+network = nx.DiGraph()
+
+network.add_nodes_from(inputlivingthings(int(input('Number of living things in this area: '))))
+print(f"This area has {network.number_of_nodes()} living things.")
+
 network.add_edges_from(inputnetwork())
+colist = ["gold","red","violet","pink","green","violet","orange","grey","blue","yellow","cyan"]
 color_list = [random.choice(colist) for _ in range(network.number_of_nodes())]
-findpath()
+
+print(findpath(input('Source: ').lower(),input('Destination: ').lower()))
+
 plt.figure(figsize=(8, 6))
-plt.title('Huang So R Han', size=10)
+plt.title('Food chains', size=10)
 nx.draw_networkx(network,node_color=color_list,with_labels=True,arrows=True,arrowstyle='->',arrowsize=20)
 plt.show()
