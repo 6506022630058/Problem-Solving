cbe1f6d192333c7bd0218909743009553d05500b
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-03-19 18:24:02 +0700

    u

--- test2.py
+++ test2.py
@@ -1,3 +1,9 @@
-def swap(l):return[l[1],l[0]]
-
-print(swap('deer tree'.split()))
+def relation(lt):
+    lisedg = [('snake', 'eagle'), ('wolf', 'eagle'), ('rat', 'eagle'), ('frog', 'eagle'), ('bird', 'eagle'), ('rat', 'snake'), ('frog', 'snake'), ('wolf', 'snake'), ('rat', 'wolf'), ('bird', 'wolf'), ('dragonfly', 'bird'), ('fruitfly', 'bird'), ('grasshopper', 'rat'), ('grasshopper', 'frog'), ('butterfly', 'frog'), ('dragonfly', 'frog'), ('fruitfly', 'frog'), ('butterfly', 'dragonfly'), ('fruitfly', 'dragonfly'), ('corn', 'grasshopper'), ('flower', 'butterfly'), ('mango', 'fruitfly')]
+    liseat = []
+    liseaten = []
+    for i in lisedg:
+        if i[0] == lt:
+            liseaten.append(i[1])
+        elif i[1] == lt:
+            liseat.append(i[0])
